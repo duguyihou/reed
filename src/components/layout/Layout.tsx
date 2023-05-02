d1b248f04140ed8f8ac0b90bd697193ea215da77
kong <duguyihou@gmail.com>
2023-05-03 08:46:15 +1000

    feat(layout): wip

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,11 +9,12 @@ import {
 import React, {PropsWithChildren, memo, useLayoutEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {HomeStackNavigationProps} from '../../routes';
+import {theme} from '../../shared/theme';
 
 type Options = {headerTitle: string};
 type Props = PropsWithChildren & Options;
 
-const TITLE_CONTAINER_HEIGHT = 60;
+const TITLE_CONTAINER_HEIGHT = 45;
 const isSameHeaderTitle = (prev: Props, next: Props) =>
   prev.headerTitle === next.headerTitle;
 
@@ -22,16 +23,19 @@ const Layout = (props: Props) => {
   const [title, setTitle] = useState('');
   const navigation = useNavigation<HomeStackNavigationProps>();
   useLayoutEffect(() => {
-    navigation.setOptions({title});
+    navigation.setOptions({
+      title,
+      headerShadowVisible: false,
+    });
   });
 
   const handleScroll = ({
     nativeEvent,
   }: NativeSyntheticEvent<NativeScrollEvent>) => {
     const {
-      contentOffset: {y},
+      contentOffset: {y: scrollOffsetY},
     } = nativeEvent;
-    setTitle(y >= TITLE_CONTAINER_HEIGHT ? headerTitle : '');
+    setTitle(scrollOffsetY >= TITLE_CONTAINER_HEIGHT ? headerTitle : '');
   };
 
   return (
@@ -52,14 +56,13 @@ export default memo(Layout, isSameHeaderTitle);
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
-    height: '100%',
+    backgroundColor: theme.white,
   },
   titleContainer: {
     height: TITLE_CONTAINER_HEIGHT,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    backgroundColor: 'red',
   },
   title: {
     fontSize: 36,
---
+++ src/shared/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  primary: '#111619',
+  secondary: '#ECF0F1',
+  white: '#FFFFFF',
+  black: '#000000',
+  red: '#f54338',
+};
